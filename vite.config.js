e80adf00f37f03e22d01d4c0ba972255db9d7bc0
16638942227 <2956061117@qq.com>
2024-04-09 08:23:14 +0800

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import UnoCss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), UnoCss()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: "[ext]/[name]-[hash].[ext]",
+      },
+    },
+  },
+
+  server: {
+    port: 5173,
+    hmr: true,
+    host: true,
+    proxy: {},
+  },
+});
